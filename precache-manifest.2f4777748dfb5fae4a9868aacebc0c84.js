07e274e6608298894c58cfdf3975417cd8627ee4
Davide Minicelli <davide.minicelli@gmail.com>
2020-09-16 17:36:41 +0100

    Updates

---
+++ precache-manifest.2f4777748dfb5fae4a9868aacebc0c84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbaa653dba5dbc41f8bbdc840eb9a0c",
+    "url": "/dminicelli-react/index.html"
+  },
+  {
+    "revision": "94a86f7a64a461e703a1",
+    "url": "/dminicelli-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa09ecf89c13196cd422",
+    "url": "/dminicelli-react/static/js/2.19bc78cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dminicelli-react/static/js/2.19bc78cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a86f7a64a461e703a1",
+    "url": "/dminicelli-react/static/js/main.b8308872.chunk.js"
+  },
+  {
+    "revision": "34960912611e72fbfc1d",
+    "url": "/dminicelli-react/static/js/runtime-main.0977c37b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dminicelli-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
